65a1bf8f3ee86617272d9c0ace6fec1ac67ebd13
Johannes <Johannes@192.168.0.2>
2019-05-11 16:08:12 +0200

    testing service for the graph 1

--- graph-impl/src/main/java/de/btu/swt/graph/impl/GraphServiceImpl.java
+++ graph-impl/src/main/java/de/btu/swt/graph/impl/GraphServiceImpl.java
@@ -8,7 +8,7 @@ package de.btu.swt.graph.impl;
 import de.btu.swt.graph.api.GraphModel;
 import de.btu.swt.graph.api.GraphService;
 import org.openide.util.lookup.ServiceProvider;
-import de.btu.swt.graph.api.Schema;
+import java.util.Arrays;
 /**
  *
  * @author Sebastian Brueggemann <bruegber@b-tu.de>
@@ -17,7 +17,7 @@ import de.btu.swt.graph.api.Schema;
 public class GraphServiceImpl implements GraphService {
 
       // root Node + Model
-       Parent root = new Parent(0,"root");
+       Parent root = new Parent(1,"root");
       GraphModelImpl model = new GraphModelImpl(root);
       
     @Override
@@ -30,20 +30,22 @@ public class GraphServiceImpl implements GraphService {
       
       
       //other Nodes: 2 Parent-nodes + 4 Leaf-nodes
-      Parent p1 = new Parent (1, "p1");
+      Parent p1 = new Parent (99, "p1");
       Parent p2 = new Parent (2, "p2");
-      /*Leaf l1 = new Leaf (3, "l1");
+      Leaf l1 = new Leaf (3, "l1");
       Leaf l2 = new Leaf (4, "l2");
       Leaf l3 = new Leaf (5, "l3");
-      Leaf l4 = new Leaf (6, "l4");*/
+      Leaf l4 = new Leaf (6, "l4");
       
       // put them into the model
-      model.addNode(p1, root);
-      model.addNode(p2, root);
-      /*model.addNode(l1, p1);
-      model.addNode(l2, p1);
-      model.addNode(l3, p2);
-      model.addNode(l4, p2);*/
+      p1.setParent(root);
+      //p2.setParent(root);
+      //model.addNode(p1, root);
+      //model.addNode(p2, root);
+      //model.addNode(l1, p1);
+     // model.addNode(l2, p1);
+      //model.addNode(l3, p2);
+      //model.addNode(l4, null);
     }
 
     @Override
